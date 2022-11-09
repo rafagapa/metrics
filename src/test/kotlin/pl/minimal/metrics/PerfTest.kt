0569b0c0054d6234de835587173f5d459e7daf83
Rafał Golcz <rgl@touk.pl>
2022-11-09 15:51:33 +0100

    Move PerfTest to test

--- src/test/kotlin/pl/minimal/metrics/PerfTest.kt
+++ src/test/kotlin/pl/minimal/metrics/PerfTest.kt
@@ -8,8 +8,10 @@ import kotlin.random.Random
 private var running = true
 
 fun main() {
+    val port = 4445
+    val receiver = LogsReceiver(4445, LogsFileWriter("/tmp/metrics/"))
     val senders = (0 until 100).map { UUID.randomUUID() }.map { guid ->
-        guid to UdpLogsSender(guid, InetAddress.getLocalHost(), 4445)
+        guid to UdpLogsSender(guid, InetAddress.getLocalHost(), port)
     }
     while (running) {
         val (guid, sender) = senders.random()
@@ -19,5 +21,6 @@ fun main() {
     Runtime.getRuntime().addShutdownHook(Thread {
         running = false
         senders.forEach { it.second.close() }
+        receiver.close()
     })
 }
